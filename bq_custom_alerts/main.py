4ba135394c779a85c184f6ac3b3734a62d3b9c84
Deepak kumar <deepakk073@gmail.com>
2025-05-17 18:32:06 -0500

    BQ AdminPOC

--- bq_custom_alerts/main.py
+++ bq_custom_alerts/main.py
@@ -35,7 +35,7 @@ def index():
     content = request.json
     try:
         print(content)
-        ds = content['resource']['labels']['dataset_id']
+        ds = content['resource']['labes']['dataset_id']
         proj = content['resource']['labels']['project_id']
         tbl = content['protoPayload']['resourceName']
         rows = int(content['protoPayload']['metadata']['tableDataChange']['insertedRowsCount'])
